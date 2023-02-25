2f6e2f8bc75f60cd82b8b69692790024721012ec
Eric Yager <eyager@amazon.com>
2023-02-25 15:59:44 -0500

    added canned auth from amplify. Planning to deprecate canned auth to share auth with proversivity

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,16 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "barkeatersridgewebap48b9ee34": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    },
+    "userPoolGroups": {
+      "BarkeatersRidgeWebappUserpoolGroupRole": "string"
+    }
+  }
+}
